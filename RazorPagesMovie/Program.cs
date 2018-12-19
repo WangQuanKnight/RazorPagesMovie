c60080fbb7ed804aad9d566cbd590a09bcfd13cd
knight <w1520138768@outlook.com>
2018-12-19 16:56:23 +0800

    2018121902

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie
 {
@@ -14,7 +17,26 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            IWebHost host = CreateWebHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    RazorPagesMovieContext context = serviceProvider.GetRequiredService<RazorPagesMovieContext>();
+                    context.Database.Migrate();
+
+                    SeedData.Initialize(serviceProvider);
+                }
+                catch (Exception ex)
+                {
+                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run(); 
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
